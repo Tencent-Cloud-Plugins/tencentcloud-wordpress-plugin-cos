a6e56b4c9b52879881a3aee0a45a418716029bb8
tuuna <550566181@qq.com>
2022-06-09 14:56:51 +0800

    user-agent统一为wordpress

--- tencentcloud-cos/common/vendor/guzzlehttp/guzzle/src/functions.php
+++ tencentcloud-cos/common/vendor/guzzlehttp/guzzle/src/functions.php
@@ -133,11 +133,7 @@ function default_user_agent()
     static $defaultAgent = '';
 
     if (!$defaultAgent) {
-        $defaultAgent = 'GuzzleHttp/' . Client::VERSION;
-        if (extension_loaded('curl') && function_exists('curl_version')) {
-            $defaultAgent .= ' curl/' . \curl_version()['version'];
-        }
-        $defaultAgent .= ' PHP/' . PHP_VERSION;
+        $defaultAgent = 'Wordpress';
     }
 
     return $defaultAgent;
